9da41a8096beced89772cffbb0126bce3a39ce9a
AmaraU <apu24@drexel.edu>
2024-09-02 13:09:02 +0100

    Accounts Page

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -9,7 +9,7 @@ export const ProfilePage = () => {
     
     return (
         <div className={styles.whole}>
-            <Text fontSize='24px' fontWeight={700} color='#101828' mb='16px'>Airtime and Bills</Text>
+            <Text fontSize='24px' fontWeight={700} color='#101828' mb='16px'>My Account</Text>
             <Tabs>
                 <TabList borderBottom={'none'} gap={'5px'} mb={'24px'}>
                     <Tab rounded='50px' fontSize='13px' color='#667085' fontWeight={500} border='1px solid #EAECF0' py='12px' px='14px'  _selected={{ color: '#FFFFFF', bg: '#667085', border: '1px solid transparent', boxShadow: '0px 0px 1px 0px #00000066'}}>Account Information</Tab>
